b3e33ede34f5f56e75f1a43ca34e323c31daf4fc
Sellin <you@example.com>
2020-12-23 13:03:00 +0100

    outline

---
+++ js1_uppg1/script.js
@@ -0,0 +1,4 @@
+const form = document.getElementById('form');
+const namn = document.getElementById('namn');
+const efternamn = document.getElementById('efternamn');
+const email = document.getElementById('email');
