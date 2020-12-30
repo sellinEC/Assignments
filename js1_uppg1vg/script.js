a698f78c981dffd418b9e5f78ad946a8e629b4b1
Sellin <you@example.com>
2020-12-30 13:25:28 +0100

    working with edits

--- js1_uppg1vg/script.js
+++ js1_uppg1vg/script.js
@@ -1,32 +1,34 @@
 let users = [
-  // {
-  //   id: "1",
-  //   namn: 'Förnamn',
-  //   efternamn: 'Efternamn',
-  //   email: 'förnamn.efternamn@mail.com'
-  // },
-  // {
-  //   id: "2",
-  //   namn: 'Förnamn2',
-  //   efternamn: 'Efternamn2',
-  //   email: 'förnamn2.efternamn2@mail.com'
-  // },
-  // {
-  //   id: "3",
-  //   namn: 'Förnamn3',
-  //   efternamn: 'Efternamn3',
-  //   email: 'förnamn3.efternamn3@mail.com'
-  // }
+  {
+    id: "1",
+    namn: 'Förnamn',
+    efternamn: 'Efternamn',
+    email: 'förnamn.efternamn@mail.com'
+  },
+  {
+    id: "2",
+    namn: 'Förnamn2',
+    efternamn: 'Efternamn2',
+    email: 'förnamn2.efternamn2@mail.com'
+  },
+  {
+    id: "3",
+    namn: 'Förnamn3',
+    efternamn: 'Efternamn3',
+    email: 'förnamn3.efternamn3@mail.com'
+  }
 ]
 
 const form = document.getElementById('form');
 const button = document.getElementById('button')
+const saveBtn = document.getElementById('save')
 const namn = document.getElementById('namn');
 const efternamn = document.getElementById('efternamn');
 const email = document.getElementById('email');
 const output = document.querySelector('#users');
-const edit = document.getElementById('edit')
+
 // const deleted = document.querySelector('#delete')
+
 
 
 button.addEventListener('click', (e) => {
@@ -63,10 +65,13 @@ function checkInputs() {
     setErrorFor(email, 'Skriv din email')
   } else if (!isEmail(emailInput)) {
     setErrorFor(email, 'Ogiltig mailadress')
-
-
   } else {
-    setSuccessFor(email);
+    if (isUser(emailInput)) {
+      setErrorFor(email, 'Användaren existerar redan')
+    } else {
+      setSuccessFor(email);
+    }
+
   }
 
 
@@ -83,6 +88,60 @@ function checkInputs() {
   }
 
 }
+
+saveBtn.addEventListener('click', (e) => {
+  e.preventDefault()
+  checkEditedInputs()
+})
+
+function checkEditedInputs() {
+  const namnInput = namn.value.trim()
+  const efternamnInput = efternamn.value.trim()
+  const emailInput = email.value.trim()
+
+  if (namnInput === '') {
+    setErrorFor(namn, 'Du måste skriva ditt namn')
+  } else {
+    setSuccessFor(namn);
+  }
+  if (efternamnInput === '') {
+
+    setErrorFor(efternamn, 'Du måste skriva ditt efternamn')
+  } else {
+
+    setSuccessFor(efternamn);
+  }
+
+  if (emailInput === '') {
+
+    setErrorFor(email, 'Skriv din email')
+  } else if (!isEmail(emailInput)) {
+    setErrorFor(email, 'Ogiltig mailadress')
+
+  } else if (isUser(emailInput)) {
+    console.log('mails');
+    setErrorFor(email, 'Användaren redan registrerad')
+
+
+  } else {
+    setSuccessFor(email);
+  }
+
+
+  //add user:
+  let elementsArray = document.getElementsByClassName("success");
+
+  if (elementsArray.length === 3) {
+    addEditedUser()
+    namn.value = ''
+    efternamn.value = ''
+    email.value = ''
+
+
+  }
+
+}
+
 
 function setErrorFor(input, message) {
   const formControl = input.parentElement;
@@ -102,6 +161,16 @@ function setSuccessFor(input) {
 function isEmail(email) {
   return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
 }
+
+function isUser(input) {
+  users.forEach(user => {
+    if (user.email === input) {
+      return true
+    }
+  });
+}
+
+
 
 function listUsers() {
   output.innerHTML = ''
@@ -128,6 +197,17 @@ function addUser() {
   listUsers();
 }
 
+function addEditedUser(id) {
+  let user = {
+    id: id,
+    namn: namn.value.trim(),
+    efternamn: efternamn.value.trim(),
+    email: email.value.trim()
+  }
+  users.push(user);
+  listUsers();
+}
+
 output.addEventListener('click', (e) => {
   if (e.target.id === "delete") {
     console.log(e.target.parentElement.parentElement.id);
@@ -135,18 +215,26 @@ output.addEventListener('click', (e) => {
     users = users.filter(user => user.id !== e.target.parentElement.id);
 
     listUsers();
+  } else if (e.target.id === "edit") {
+    users.forEach(user => {
+      if (user.id === e.target.parentElement.id) {
+        editUser(user)
+      }
+
+    });
+
+    // target = e.target.parentElement
+    // console.log(target);
+    // editUser(target)
   }
 
 })
 
-// output.addEventListener('click', (e) => {
-//   console.log(e.target.parentElement.id);
-// })
-
-function removeElement(elementId) {
-  // Removes an element from the document
-  let element = document.getElementById(elementId);
-  element.parentNode.removeChild(element);
+function editUser(target) {
+  namn.value = target.namn
+  efternamn.value = target.efternamn
+  email.value = target.email;
+  saveBtn.style.visibility = 'visible'
 }
 
 listUsers()
